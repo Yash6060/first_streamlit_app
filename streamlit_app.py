bfba5109a0c90e49b90f34a66513ab6b8a189fd2
Yash Chavan <yash.n.chavan@kipi.bi>
2022-07-24 00:09:10 +0530

    Added Snowflake Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-
+import snowflake.connector
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents new Healthy Diner')
@@ -20,3 +20,10 @@ fruit_to_show = my_fruit_list.loc[fruit_selected]
 # Display the table on the page.
 
 streamlit.dataframe(fruit_to_show)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
